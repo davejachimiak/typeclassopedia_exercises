a0984490873201a6af892547f271523fb1644c7f
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-02-22 20:45:14 -0500

    Implement Functor Pair

--- functors.hs
+++ functors.hs
@@ -9,3 +9,8 @@ instance Functor (Either' e) where
 -- commented below bc already defined in GHC.Base
 -- instance Functor ((->) a) where
 --     fmap = (.)
+
+data Pair a = Pair a a deriving Show
+
+instance Functor Pair where
+    fmap g (Pair a b) = Pair (g a) (g b)
