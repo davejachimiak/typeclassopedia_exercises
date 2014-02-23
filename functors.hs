aa7f533ac18edbd7b88082e3b700fd695a4dbb09
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-02-22 20:23:36 -0500

    Implement Functor ((->) a)

--- functors.hs
+++ functors.hs
@@ -3,3 +3,9 @@ data Either' e a = Left' e | Right' a deriving Show
 instance Functor (Either' e) where
     fmap g (Right' a) = Right' (g a)
     fmap _ (Left' e)  = Left' e
+
+-- returns function that expects one argument, e.g., if it compiled:
+-- (.) (\_ -> ((->) String))) ((->) Integer)
+-- commented below bc already defined in GHC.Base
+-- instance Functor ((->) a) where
+--     fmap = (.)
