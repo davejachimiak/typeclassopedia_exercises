f18c106a3ebfe57e5585add8b3628480f30ce523
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-03-10 09:01:45 -0400

    Explain breaking of Functor law

--- functors.hs
+++ functors.hs
@@ -48,3 +48,24 @@ instance Functor ITree where
 -- constructors that are functors take one argument, which is the
 -- argument that is mapped in fmap.
 
+-- Functor laws:
+-- fmap id = id
+-- fmap (g . h) = (fmap g) . (fmap h)
+
+-- The following is an evil functor instance. It breaks the first
+-- law. Mapping `id' over the List is not equal to calling the
+-- List with `id'.
+
+data List a = EmptyList | List [a] deriving (Show, Eq)
+
+instance Functor List where
+    fmap g (List [])   = List []
+    fmap g (List (x:xs)) = List (g x : g x : fmap g xs)
+
+-- fmap id (List ["functor", "foul"]) == id (List ["functor", "foul"])
+-- False
+--
+-- fmap id (List ["functor", "foul"])
+-- > List ["functor","functor","foul"]
+-- id (List ["functor", "foul"])
+-- > List ["functor","foul"]
