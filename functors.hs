5ddbe101526ce9384de509b4247a2ffc545268c6
Dave Jachimiak <dave.jachimiak@gmail.com>
2014-09-27 17:20:14 -0400

    Add some organization to the file

--- functors.hs
+++ functors.hs
@@ -1,3 +1,6 @@
+-- Instances
+--
+-- 1. Implement Functor instances for Either e and ((->) e).
 data Either' e a = Left' e | Right' a deriving Show
 
 instance Functor (Either' e) where
@@ -11,6 +14,10 @@ instance Functor (Either' e) where
 --     fmap = (.)
 
 -----------------------------------------------------------------
+-- 2. Implement Functor instances for ((,) e) and for Pair, defined as
+--     data Pair a = Pair a a
+--     Explain their similarities and differences.
+--
 -- Both Pair and (,) take 2 arguments to fulfill the value
 -- constructor. The difference between Pair and (,) is that Pair
 -- has a kind of * -> * and (,) has a kind of * -> * -> *.
@@ -30,6 +37,9 @@ instance Functor (Comma e) where
     fmap g (Comma e a) = Comma e (g a)
 
 -----------------------------------------------------------------
+-- 3. Implement a Functor instance for the type ITree, defined as
+--    data ITree a = Leaf (Int -> a) 
+--                 | Node [ITree a]
 
 data ITree a = Leaf (Int -> a)
              | Node [ITree a]
@@ -41,6 +51,18 @@ instance Show (ITree a) where
 instance Functor ITree where
     fmap g (Node xs) = Node $ map (fmap g) xs
     fmap g (Leaf h) = Leaf $ g . h
+--
+-----------------------------------------------------------------
+-- 4. Give an example of a type of kind * -> * which cannot be
+--    made an instance of Functor (without using undefined).
+--
+-----------------------------------------------------------------
+-- 5. Is this statement true or false?
+--
+-- The composition of two Functors is also a Functor.
+--
+-- If false, give a counterexample; if true, prove it by
+-- exhibiting some appropriate Haskell code. 
 
 -- The composition of two functors is a functor. Composition
 -- applies the second function to a parameter,
@@ -48,6 +70,10 @@ instance Functor ITree where
 -- constructors that are functors take one argument, which is the
 -- argument that is mapped in fmap.
 
+-----------------------------------------------------------------
+-----------------------------------------------------------------
+-- Laws
+--
 -- Functor laws:
 -- fmap id = id
 -- fmap (g . h) = (fmap g) . (fmap h)
